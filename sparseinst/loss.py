b2f9865e9a578077d85bf2465de8d66c4b227553
TeRiRi2333 <1965448834@qq.com>
2022-06-27 21:35:47 +0800

    commit

--- sparseinst/loss.py
+++ sparseinst/loss.py
@@ -5,4 +5,15 @@ import mindspore.ops as ops
 
 from detectron2.utils.registry import Registry
 
+SPARSE_INST_MATCHER_REGISTRY = Registry("SPARSE_INST_MATCHER")
+SPARSE_INST_MATCHER_REGISTRY.__doc__ = "Matcher for SparseInst"
+SPARSE_INST_CRITERION_REGISTRY = Registry("SPARSE_INST_CRITERION")
+SPARSE_INST_CRITERION_REGISTRY.__doc__ = "Criterion for SparseInst"
 
+
+class SparseInstMatcher(nn.Cell):
+	def __init__(self,cfg):
+		super().__init__()
+		self.alpha=cfg.MODEL.SPARSE_INST.MATCHER.ALPHA 
+		self.beta=cfg.MODEL.SPARSE_INST.MATCHER.BETA 
+		self.mask
